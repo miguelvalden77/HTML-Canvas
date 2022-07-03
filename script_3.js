08eec7f9d34db84982581815edfbff9e666ce9ee
miguelvalden <miguelvalden77@gmail.com>
2022-07-03 04:07:35 +0200

    Game Over imagen

--- script_3.js
+++ script_3.js
@@ -12,26 +12,60 @@ buttonColision.addEventListener("click", ()=>{
     contexto.fillStyle = "green"
     contexto.fillRect(150, 100, 100, 100)
 
+    let distanceLeft = 10
+    let distanceTop = 100
+
+    const hayColision = (x) =>{
+        return (x + 100) >= 150
+    }
+    
     addEventListener("keydown", e =>{
         
         let tecla = e.key
+        contexto.fillStyle = "red"
 
         if(tecla === "ArrowUp"){  
 
+            contexto.clearRect(0, 0, 400, 400)
+
+            distanceTop -= 1
+            contexto.fillRect(distanceLeft, distanceTop, 100, 100)
+
         } else if(tecla === "ArrowDown"){
+
+            contexto.clearRect(0, 0, 400, 400)
+
+            distanceTop += 1
+            contexto.fillRect(distanceLeft, distanceTop, 100, 100)
 
         } else if(tecla === "ArrowRight"){
 
             contexto.clearRect(0, 0, 400, 400)
-            contexto.fillStyle = "red"
-            contexto.fillRect(20, 100, 100, 100)
+
+            distanceLeft += 1
+            contexto.fillRect(distanceLeft, distanceTop, 100, 100)
 
         } else if(tecla === "ArrowLeft"){
+            
+            contexto.clearRect(0, 0, 400, 400)
+
+            distanceLeft -= 1
+            contexto.fillRect(distanceLeft, distanceTop, 100, 100)
 
         }
 
         contexto.fillStyle = "green"
         contexto.fillRect(150, 100, 100, 100)
+
+        if(hayColision(distanceLeft)){
+        let imagenGameOver = new Image()
+        imagenGameOver.src = "Game-over.jpg"
+
+        imagenGameOver.onload = ()=>{
+            contexto.clearRect(0, 0, 400, 400)
+            contexto.drawImage(imagenGameOver, 0, 0, 400, 400)
+        }
+    }
 
     })
 })
